6d7997138b2d6efb728c2776c1b485158f9775b9
Benoit P <benoit.patra@gmail.com>
2021-12-22 14:33:02 +0100

    update with men page

---
+++ Men's Gift Ideas _ Under Armour_files/config.js
@@ -0,0 +1,5 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C0MMHH4P76SVVJ0VAHFG").setAdvancedMatchingAvailableProperties({"email":false,"phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C0MMHH4P76SVVJ0VAHFG").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C0MMHH4P76SVVJ0VAHFG").setPixelInfo({status: 0, name: "UA.com NA Tracking Pixel", advertiserID: "6916951500648349697", setupMode: 1, partner: "", is_onsite: false, enableAutoAM: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("underarmour.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
