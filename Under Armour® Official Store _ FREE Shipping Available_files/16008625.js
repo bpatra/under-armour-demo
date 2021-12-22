984a5411130b81e88823f74339bf9bdf7976c433
Benoit P <benoit.patra@gmail.com>
2021-12-22 13:06:31 +0100

    Get under armour front page and add livechat tag

---
+++ Under Armour® Official Store _ FREE Shipping Available_files/16008625.js
@@ -0,0 +1,6 @@
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    c[a]("set","_s","uet");c[a]("set","_u","16008625");
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "6ox2gnxlpt");
